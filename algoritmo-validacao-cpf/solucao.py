853b390aefdd24eedec98d4b5134317995fb5173
willianmonteiro <willianmontteiro23@gmail.com>
2024-10-30 21:19:52 -0300

    Update algoritmo-validacao-cpf/solucao.py

--- algoritmo-validacao-cpf/solucao.py
+++ algoritmo-validacao-cpf/solucao.py
@@ -25,51 +25,52 @@ O primeiro dígito do CPF é 7
 """
 
 # Coleta o CPF sem pontos e traços e transforma os dígitos em uma lista de inteiros
-coleta_cpf = input('Digite seu CPF (apenas números): ')
+cpf_input = input('Digite seu CPF (apenas números): ')
 
 # Remove caracteres não numéricos e converte em lista de inteiros
-cpf_base = [int(digito) for digito in coleta_cpf if digito.isdigit()]
+cpf_digits = [int(digit) for digit in cpf_input if digit.isdigit()]
 
 # Verifica se o CPF tem 11 dígitos
-if len(cpf_base) != 11:
+if len(cpf_digits) != 11:
     print('CPF inválido! Deve conter 11 dígitos.')
     exit()
 
 # Verifica se o CPF é composto por números repetidos, que são inválidos
-if cpf_base == cpf_base[::-1] * len(set(cpf_base)):
+if cpf_digits == cpf_digits[::-1] * len(set(cpf_digits)):
     print('CPF inválido! Não pode ser composto por números repetidos.')
     exit()
 
 # Cálculo do primeiro dígito verificador
-multiplicador_primeiro_digito = 10
-soma_primeiro_digito = 0
+first_multiplier = 10
+first_digit_sum = 0
 for i in range(9):  # Multiplica os primeiros 9 dígitos
-    soma_primeiro_digito += cpf_base[i] * multiplicador_primeiro_digito
-    multiplicador_primeiro_digito -= 1
+    first_digit_sum += cpf_digits[i] * first_multiplier
+    first_multiplier -= 1
 
 # Multiplica por 10 e tira o módulo por 11
-numero_multiplicado_1 = soma_primeiro_digito * 10
-numero_final_1 = numero_multiplicado_1 % 11
-primeiro_digito_verificador = numero_final_1 if numero_final_1 < 10 else 0
+multiplied_number_1 = first_digit_sum * 10
+final_number_1 = multiplied_number_1 % 11
+first_check_digit = final_number_1 if final_number_1 < 10 else 0
 
 # Cálculo do segundo dígito verificador
-# Para calcular o segundo número, o primeiro dígito entra no a conta
-multiplicador_segundo_digito = 11
-soma_segundo_digito = 0
+# Para calcular o segundo número, o primeiro dígito entra na conta
+second_multiplier = 11
+second_digit_sum = 0
 for i in range(10):  # Multiplica os 9 primeiros dígitos + o primeiro dígito verificador
-    soma_segundo_digito += cpf_base[i] * multiplicador_segundo_digito
-    multiplicador_segundo_digito -= 1
+    second_digit_sum += cpf_digits[i] * second_multiplier
+    second_multiplier -= 1
 
 # Multiplica por 10 e tira o módulo por 11
-numero_multiplicado_2 = soma_segundo_digito * 10
-numero_final_2 = numero_multiplicado_2 % 11
-segundo_digito_verificador = numero_final_2 if numero_final_2 < 10 else 0
+multiplied_number_2 = second_digit_sum * 10
+final_number_2 = multiplied_number_2 % 11
+second_check_digit = final_number_2 if final_number_2 < 10 else 0
 
 # Verifica se os dígitos verificadores calculados coincidem com os fornecidos
-if primeiro_digito_verificador == cpf_base[9] and segundo_digito_verificador == cpf_base[10]:
+if first_check_digit == cpf_digits[9] and second_check_digit == cpf_digits[10]:
     print('O CPF inserido está correto')
 else:
     print('CPF inválido')
+
 
 
 
